c08b654d23f86e5b2920e7ebaaf05412ac62fb0a
mlmis <mlmis.techsupp@gmail.com>
2025-07-10 17:42:33 +0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,8 @@
+// frontend/src/services/api.js
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+});
+
+export default apiClient;
